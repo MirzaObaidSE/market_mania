5dcc6580fdac049c021480106a9f314553033b08
Mirza Obaid <mirzaobaidse@gmail.com>
2016-06-07 16:55:21 +0500

    about page corredted

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -11,8 +11,8 @@
         <!-- Introduction Row -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">About Us
-                    <small>It's Nice to Meet You!</small>
+                <h1 class="page-header">Market Mania
+                    <small>Marketing Solution</small>
                 </h1>
                 <p>Market Mania is a Marketing application and it can be used for marketing your product and event about your
                 	company by using Facebook and Twitter. Market Mania is available in very cost effect price as compared with others
@@ -23,14 +23,17 @@
         <!-- Team Members Row -->
         <div class="row">
             <div class="col-lg-12">
-                <h2 class="page-header">Our Team</h2>
+                
             </div>
-            <div class="text-center circular">
-                <img class="img-circle img-responsive img-center" src="{{asset('./img/me.jpg')}}" alt="">
+            <div class="text-center">
+                <img class="circular img-responsive img-center" src="{{asset('./img/me.jpg')}}" alt="">
                 <h3>Mirza Obaid
                     <small>Software Engineer</small>
                 </h3>
                 <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            	<b> CONTACT US </b><br>
+            	<a href="mailto:mirzaobaidse@gmail.com">mirzaobaidse@gmail.com</a> <br>
+            	+923026077069
             </div>
             
         </div>
